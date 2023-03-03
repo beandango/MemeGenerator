dff3120e07fa0e5d05c2b72c59cdf8af93e06ecd
Sarah Hughes <106944445+beandango@users.noreply.github.com>
2023-03-03 03:26:21 -0500

    initial commit

---
+++ DataPath.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def create_file_paths_txt(folder_path):
+    with open('file_paths.txt', 'w') as file:
+        for root, dirs, files in os.walk(folder_path):
+            for filename in files:
+                file.write(os.path.join(root, filename) + '\n')
+
+folder_path = os.getenv('FOLDER_PATH') # Replace FOLDER_PATH with path/to/folder
+create_file_paths_txt(folder_path)
